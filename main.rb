7f5899f3d517304e6d26876bb8011bdcc44e1d55
ZachBaird <zachbairddev@gmail.com>
2020-01-11 11:35:02 -0500

    Initial commit

---
+++ main.rb
@@ -0,0 +1,26 @@
+def substrings(string_to_count, dictionary)
+  strings_array = string_to_count.split
+  
+  results = Hash.new
+
+  dictionary.each do |potential_substring|
+    strings_array.each do |word|
+
+      if word.downcase.include? potential_substring
+        if results[potential_substring].nil?
+          results[potential_substring] = 1
+        else
+          results[potential_substring] += 1
+        end
+      end
+      
+    end
+  end
+
+  return results
+
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
